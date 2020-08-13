6813f0f6e0fecd07dc507c3456df6d9dfee6c8e1
Bahriye Begum Ultan Gungor <begumultan@gmail.com>
2020-08-13 12:55:18 +0300

    first commit

---
+++ LandmarkBook/ImageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ImageViewController.swift
+//  LandmarkBook
+//
+//  Created by Bahriye Begum Ultan Gungor on 13.08.2020.
+//  Copyright © 2020 Bahriye Begum Ultan Gungor. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var landmarkLabel: UILabel!
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -8,13 +8,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    var chosenLanmarkName = ""
+    var chosenLanmarkImage = UIImage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Landmark Book Data
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonehenge")
+        landmarkNames.append("Tajmahal")
+        
+        landmarkImages.append(UIImage(named: "colosseum")!)
+        landmarkImages.append(UIImage(named: "greatwall")!)
+        landmarkImages.append(UIImage(named: "kremlin")!)
+        landmarkImages.append(UIImage(named: "stonehenge")!)
+        landmarkImages.append(UIImage(named: "tajmahal")!)
+        
+        navigationItem.title = "Landmark Book"
+    }
+    
+    // for tableview delegate, datasource
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = landmarkNames[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarkNames.count
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenLanmarkName = landmarkNames[indexPath.row]
+        chosenLanmarkImage = landmarkImages[indexPath.row]
+        
+        performSegue(withIdentifier: "toImageVC", sender: nil)
+    }
 
+    // segue olmadan önce çalışacak
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toImageVC"{
+            // casting : as
+            let destinationVC = segue.destination as! ImageViewController
+            destinationVC.selectedLandmarkName = chosenLanmarkName
+            destinationVC.selectedLandmarkImage = chosenLanmarkImage
+        }
+    }
+
+    
+    
+    
 }
+
+
+
+
+
+
+
 
